204df4a3457f6b3565bb93c1ee8176d1fcb98330
rmgarcia000 <36377877+rmgarcia000@users.noreply.github.com>
2018-09-09 05:00:42 -0300

    iniciando projeto

---
+++ api/transferencias-financeiras-api/src/main/java/br/com/transferenciasfinanceirasapi/TransferenciasFinanceirasApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.transferenciasfinanceirasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciasFinanceirasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciasFinanceirasApiApplication.class, args);
+	}
+}
